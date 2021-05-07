d9c93c5e4b352014c69a33abe5196efd8ce5a66c
Vaishnavi Kulkarni <vaishk098@gmail.com>
2021-05-07 14:38:01 +0530

    Added UserDao and DaoException

---
+++ src/main/java/com/sapient/dao/UserDao.java
@@ -0,0 +1,140 @@
+package com.sapient.dao;
+
+import java.sql.*;
+
+import com.sapient.utils.DbUtil;
+
+public class UserDao {
+
+	public Boolean addNewUser(String name, String email, String password, Integer isProvider, Double balance)
+			throws DaoException {
+		String sql = "INSERT INTO users (NAME, EMAIL,PASSWORD,BALANCE) VALUES (?,?,?,?)";
+		//what to do about user id generation?
+		try (Connection conn = DbUtil.createConnection(); PreparedStatement stmt = conn.prepareStatement(sql);) {
+			stmt.setString(1, name);
+			stmt.setString(2, email);
+			stmt.setString(3, password);
+			stmt.setInt(4, isProvider);
+			stmt.setDouble(5, balance);
+
+			stmt.executeUpdate();
+			System.out.println("new user added");
+			return true;
+
+		} catch (Exception e) {
+			throw new DaoException(e);
+		}
+	}
+
+	public double getBalance(Integer userId) throws DaoException, ClassNotFoundException, SQLException {
+		Double myBalance = 0.0;
+		String sql = "SELECT * FROM users WHERE ID = ?";
+		try (Connection conn = DbUtil.createConnection(); PreparedStatement stmt = conn.prepareStatement(sql);) {
+			stmt.setInt(1, userId);
+			try (ResultSet rs = stmt.executeQuery();) {
+				if (rs.next()) {
+					myBalance = rs.getDouble("BALANCE");
+
+				} else {
+					System.out.println("No data found!");
+				}
+
+			} catch (Exception e) {
+				throw new DaoException(e);
+			}
+			return myBalance;
+		}
+	}
+	
+	public Boolean addToWallet(Integer userId, Double amount) throws DaoException {
+		Double currentBalance = 0.0;
+		String sql = "SELECT * FROM users WHERE ID =  ?";
+		try (Connection conn = DbUtil.createConnection(); PreparedStatement stmt = conn.prepareStatement(sql);) 
+		{
+			stmt.setInt(1, userId);
+			try(ResultSet rs = stmt.executeQuery();
+					)
+			{
+				if(rs.next()) {
+					currentBalance = rs.getDouble("BALANCE");
+				}
+				else
+				{
+					System.out.println("No data found!"); 
+				}
+			
+		   }
+				
+		}
+		catch (Exception e) {
+			throw new DaoException(e);
+		}
+		
+		String sql2 = "UPDATE users SET BALANCE = ? WHERE ID = ?";
+		try (Connection conn = DbUtil.createConnection(); PreparedStatement stmt = conn.prepareStatement(sql2);) 
+		{
+			stmt.setDouble(1, amount+currentBalance);
+			stmt.setInt(2,userId);
+			stmt.executeUpdate(); 
+			System.out.println("Wallet updated");
+			return true;
+		}
+		catch (Exception e) 
+		{
+			throw new DaoException(e);
+		}
+		
+	}
+	
+	public Boolean withdrawFromWallet(Integer userId, Double amount) throws DaoException
+	{
+		Double currentBalance = 0.0;
+		String sql = "SELECT * FROM users WHERE ID =  ?";
+		try (Connection conn = DbUtil.createConnection(); PreparedStatement stmt = conn.prepareStatement(sql);) 
+		{
+			stmt.setInt(1, userId);
+			try(ResultSet rs = stmt.executeQuery();
+					)
+			{
+				if(rs.next()) {
+					currentBalance = rs.getDouble("BALANCE");
+				}
+				else
+				{
+					System.out.println("No data found!"); 
+				}
+			
+		   }
+				
+		}
+		catch (Exception e) {
+			throw new DaoException(e);
+		}
+		
+		if(currentBalance>=amount)
+		{
+			String sql2 = "UPDATE users SET BALANCE = ? WHERE ID = ?";
+			try (Connection conn = DbUtil.createConnection(); PreparedStatement stmt = conn.prepareStatement(sql2);) 
+			{
+				stmt.setDouble(1, currentBalance - amount);
+				stmt.setInt(2,userId);
+				stmt.executeUpdate(); 
+				System.out.println("Wallet updated");
+				return true;
+			}
+			catch (Exception e) 
+			{
+				throw new DaoException(e);
+			}
+			
+		}
+		else
+		{
+			System.out.println("Insufficient balance");
+		}
+		
+		return null;
+		
+	}
+	
+}
