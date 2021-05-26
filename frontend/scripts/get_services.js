56d9d6d2d59377042e5cb6f56093e820f42c598c
Sidharth Sunilkumar <sidharth.sunilkumar@gmail.com>
2021-05-26 12:59:25 +0400

    some modifications

--- frontend/scripts/get_services.js
+++ frontend/scripts/get_services.js
@@ -8,24 +8,25 @@ $(document).ready(function(){
             console.log(data)
             $.each( data.services, function( service ) {
                 $("#serviceBlockId").append(
-                    `<div>
+                    `<div style="margin:1%">
                         <div class="card">
                             <h5 class="card-title"  style="padding:1%;height:100%;width:100%">
-                                <div style="padding:1%;float: left;height:200px;width:15%">
-                                    <img src=${data.services[service]['imageUrl']} alt="No image" width="100" height="100">
-                                </div>
-                                <div style="padding:1%;;width:80%">
-                                    <h3>${data.services[service]['name']}</h4><br> 
-                                    ${data.services[service]['description']}<br> 
-                                    Price- ${data.services[service]['price']}<br> 
-                                    Ratings- ${data.services[service]['ratings']}<br> 
-                                    <div style="margin:15px;">
-                                        <button id="message_btn" class="btn btn-success">Message</button>
-                                        <button id="book_btn" class="btn btn-success">Book</button>    
+                                <div>
+                                    <div style="float:left;height:200px;width:150px;">
+                                        <img src=${data.services[service]['imageUrl']} alt="No image" width="120" height="120">
+                                    </div>
+                                    <div style="height:200px">
+                                        <h3>${data.services[service]['name']}</h4>
+                                        <hr style="height:2px;">
+                                        ${data.services[service]['description']}<br> 
+                                        Price- ${data.services[service]['price']}<br> 
+                                        Ratings- ${data.services[service]['ratings']}<br> 
                                     </div>
                                 </div>
-                                
-
+                                <div>
+                                    <button id="message_btn" class="btn btn-success" style="margin-left:30px;width:100px">Message</button>
+                                    <button id="book_btn" class="btn btn-success" style="margin-left:30px;width:100px">Book</button>
+                                </div>
                             </h5>
                         </div>
                     </div>`
